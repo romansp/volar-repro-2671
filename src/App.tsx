7b7f2b01e9dad0228291f9283f73e32fb6ec5caf
Raman Paulau <raman.paulau@axure.com>
2023-04-27 10:41:06 -0700

    repro #2671, #2674

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import ButtonLink from './components/ButtonLink.vue'
+
+const App = 
+  <ButtonLink text="My Link" title="My Title" />
+
+export default App;
---
+++ src/components/ButtonLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+}>()
+</script>
+
+<template>
+  <button title="Button Title">
+    {{ text }}
+  </button>
+</template>
